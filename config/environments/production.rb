2817039db8c48ca6c73a944deff92c55b6964c71
Jordan T. Cox <jcox@nerdery.com>
2012-10-05 14:29:51 -0500

    Removed asset precompilation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ ComPhantomdataMonicle::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true # Wtf?  Investigate later.
 
   # Generate digests for assets URLs
   config.assets.digest = true
