941ae1719e0f42b94bc53cebe8210af6cc5a9ef6
Jordan T. Cox <jordan.t.cox@gmail.com>
2013-01-22 20:03:09 -0600

    Adding editing and listing for alarms

--- app/controllers/alarms_controller.rb
+++ app/controllers/alarms_controller.rb
@@ -8,11 +8,35 @@ class AlarmsController < ApplicationController
 			return render :new
 		end
 		raise "That's not your sensor!" if current_user.sensors.where(:id => @alarm.sensor_id).first.nil?
-		raise @alarm.inspect
+		@alarm.save!
+		flash[:notice] = "Alarm saved."
+		return redirect_to alarms_path		
+	end
+	def index
+		@alarms = current_user.sensors.map { |sensor| sensor.alarms }.flatten.compact		
+	end
+	def edit
+		@alarm = Alarm.find_by_id params[:id]
+		@sensors = current_user.sensors
+		@trigger_types = Alarm::TRIGGER_TYPES
+		raise "That's not your alarm!" if current_user.sensors.where(:id => @alarm.sensor_id).first.nil?
 	end
 	def new
 		@alarm = Alarm.new
 		@sensors = current_user.sensors
 		@trigger_types = Alarm::TRIGGER_TYPES
+	end
+	def update
+		@alarm = Alarm.find_by_id params[:id]
+		@alarm.update_attributes(params[:alarm])
+		unless @alarm.valid?
+			@sensors = current_user.sensors
+			@trigger_types = Alarm::TRIGGER_TYPES
+			return render :edit
+		end
+		raise "That's not your sensor!" if current_user.sensors.where(:id => @alarm.sensor_id).first.nil?
+		@alarm.save!
+		flash[:notice] = "Alarm saved."
+		return redirect_to alarms_path		
 	end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 	def error_messages(obj)
     	if obj.errors.any?
-      		msg =  '<div class="alert error">'
+      		msg =  '<div class="alert danger">'
       		msg << '	' + obj.errors.full_messages.map { |msg| "<p>#{sanitize(msg)}</p>" }.join + ''
       		msg << '</div>'
       		raw(msg)
