0ed037ef57d29616b728bfc8cc0c1c812cc5f156
Jordan T. Cox <jordan.t.cox@gmail.com>
2013-11-19 22:08:08 -0600

    Moving secret token out to config entry

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-PDMonocle::Application.config.secret_key_base = '897e62f6eb4f4425f90a7e177fdfa67793bc55071c260f9ba39e6fc00cb4f1d1b84cd345e6e4a218d2889312e95c9aa4bab91404c92b181809eb942540790a70'
+PDMonocle::Application.config.secret_key_base = APP_CONFIG['secret_token'] || ENV['M_SECRET_TOKEN']
