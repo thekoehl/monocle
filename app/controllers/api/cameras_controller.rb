5bd9f0409241806cb9468b2c39fb7dfdcdf7850c
Jordan T. Cox <jordan.t.cox@gmail.com>
2013-11-20 01:32:17 -0600

    converting a bunch of tabs to spaces

--- app/controllers/api/cameras_controller.rb
+++ app/controllers/api/cameras_controller.rb
@@ -1,28 +1,28 @@
 class Api::CamerasController < Api::BaseController
-	before_filter :load_and_authenticate_api_user!
-	protect_from_forgery :except => :create # This is meant to be called from the outside
+  before_filter :load_and_authenticate_api_user!
+  protect_from_forgery :except => :create # This is meant to be called from the outside
 
-	# This really is a create or update; but this is easiest for API
-	# users to use.
-	def create
-		validate_create_params
+  # This really is a create or update; but this is easiest for API
+  # users to use.
+  def create
+    validate_create_params
 
-		@camera = @current_user.cameras.find_or_create_by(name: params[:camera][:name], user_id: @current_user)
-		@camera.latest_snapshot = params[:latest_snapshot]
-		@camera.save!
+    @camera = @current_user.cameras.find_or_create_by(name: params[:camera][:name], user_id: @current_user)
+    @camera.latest_snapshot = params[:latest_snapshot]
+    @camera.save!
 
-		render json: json_success
-	end
+    render json: json_success
+  end
 
-	def index
-		@cameras = @current_user.cameras
-	end
+  def index
+    @cameras = @current_user.cameras
+  end
 
 private
 
-	def validate_create_params
-		raise "No camera passed" unless params[:camera]
-		raise "No name passed for the camera" unless params[:camera][:name]
-	end
+  def validate_create_params
+    raise "No camera passed" unless params[:camera]
+    raise "No name passed for the camera" unless params[:camera][:name]
+  end
 
 end
--- app/models/camera_event.rb
+++ app/models/camera_event.rb
@@ -1,8 +1,8 @@
 class CameraEvent < ActiveRecord::Base
-	has_attached_file :event_recording
+  has_attached_file :event_recording
 
-	belongs_to :user
+  belongs_to :user
 
-	validates :event_recording, attachment_presence: true
-	validates :location, presence: true
+  validates :event_recording, attachment_presence: true
+  validates :location, presence: true
 end
