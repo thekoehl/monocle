e0e4900d08a125a887365dbf51e2e44d2cc56087
Jordan T. Cox <jordan.t.cox@gmail.com>
2013-12-12 02:17:16 -0600

    re-ordering scifi names

--- app/controllers/sensors_controller.rb
+++ app/controllers/sensors_controller.rb
@@ -7,8 +7,8 @@ class SensorsController < ActionController::Base
 
   def scifi
     @cameras = current_user.cameras
-    @numeric_sensors = current_user.numeric_sensors
-    @stateful_sensors = current_user.stateful_sensors
+    @numeric_sensors = current_user.numeric_sensors.order('name ASC')
+    @stateful_sensors = current_user.stateful_sensors.order('name ASC')
     render layout: false
   end
 
