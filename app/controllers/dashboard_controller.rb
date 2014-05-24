47326cf0152136038db5b62b3a0152abc4e235a8
Jordan T. Cox <jordan.t.cox@gmail.com>
2014-05-24 00:00:46 -0500

    Reworking the visuals

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,6 @@
 class DashboardController < ApplicationController
   before_filter :authenticate_user!
-  def index; end
+  def index
+    @groups = current_user.groups.includes(:sensors)
+  end
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -4,4 +4,12 @@ class Group < ActiveRecord::Base
 
   belongs_to :user
   has_many :sensors, -> {order('name ASC')}, dependent: :destroy
+
+  # Why not just use sensors.any(needs_attention: true)?  The context of this method
+  # is meant to be in a group setting.  You should pre-fetch the sensors with:
+  #      groups.includes(:sensors)
+  def needs_attention?
+    sensors.find {|s| return true if s.needs_attention }
+    return false
+  end
 end
