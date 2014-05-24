0e76a63bfaad2da54edbabcbb8e1d306fdf81fdd
Jordan T. Cox <jordan.t.cox@gmail.com>
2014-05-23 22:21:16 -0500

    Trimming up routes; skinning the sign in page a bit

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,6 @@
 class StaticPagesController < ApplicationController
   def landing_page
     return redirect_to '/dashboard' if current_user
+    return redirect_to new_user_session_path
   end
 end
