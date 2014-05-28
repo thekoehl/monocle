35a569e9dfad6e3a1241ff22faa95d113a297735
Jordan T. Cox <jordan.t.cox@gmail.com>
2014-05-27 23:36:29 -0500

    Confining graph height to be more visually appealing

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@ puts "Destroying previous data..."
 Group.destroy_all
 puts "Rebuilding..."
 
-(0..5).each do |group_number|
+(0..1).each do |group_number|
   g = Group.new
   g.user = u
   g.name = "Group #{group_number}"
@@ -15,14 +15,14 @@ puts "Rebuilding..."
     s.name = "Sensor #{sensor_number}"
     s.units = "F"
     s.save!
-    (0..720).each do |hour_from_now|
+    (0..48).each do |hour_from_now|
       [0].each do |minutes_from_now|
         t = Time.now - hour_from_now.hours
         t -= minutes_from_now.minutes
         d = DataPoint.new
         d.sensor = s
         d.logged_at = t.to_s
-        d.value = Random.rand(100)
+        d.value = Random.rand(500)
         d.save!
       end
     end
